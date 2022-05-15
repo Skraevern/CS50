9994162e3042859eec5f2d4aa2bb33a0e345e99f
Skraevern <86933999+Skraevern@users.noreply.github.com>
2022-05-16 00:40:48 +0200

    Revert "test"
    This reverts commit 8bd958295ad91104c69dd5c0d6913f79b25033e8.

--- week-02/problem-set-02/caesar.c
+++ week-02/problem-set-02/caesar.c
@@ -3,67 +3,66 @@
 #include <string.h>
 #include <stdlib.h>
 
-bool only_digits(string s); 
+bool only_digits(string s);
 
 int main(int argc, string argv[])
-{   
-    if (argv[1] != NULL || argv[2] == NULL)
-    {
-       return 0;
-    }
-    else
+{
+    if (argv[1] == NULL)
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
-    if (only_digits(argv[1]) == true)
-    {
-        return 0;
-    }
-    else
+    if (argv[2] != NULL)
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
+    bool is_digit = only_digits(argv[1]);
+    if (is_digit == 0)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+
     int cypher = atoi(argv[1]);
     string text = get_string("plaintext:  ");
     int letter;
-            
+
     for (int i = 0; i < strlen(text); i++)
     {
         letter = text[i];
         if (letter >= 97 && letter <= 122)
         {
             letter = text[i] + cypher;
-            for (int i = 0; letter >= 122; i++)
+            for (int in = 0; letter >= 122; in++)
             {
                 letter = letter - 26;
             }
         }
         if (letter >= 65 && letter <= 90)
         {
-            letter = text[i] + cypher;
-            for (int i = 0; letter >= 90; i++)
+        letter = text[i] + cypher;
+        for (int ind = 0; letter >= 90; ind++)
             {
                 letter = letter - 26;
             };
         }
-
         text[i] = letter;
     }
     printf("ciphertext: %s\n", text);
+    return 0;
 }
 
 
-bool only_digits(string s) 
+bool only_digits(string s)
 {
-    int ascii = s[0];
-    if (ascii >= 48 && ascii <= 57)
+    for (int i = 0; i < strlen(s); i++)
     {
-        return true;
+        int ascii = s[i];
+        if (ascii < 48 || ascii > 57)
+        {
+            return false;
+        }
     }
-    else
-    {
-        return false;
-    }
+    return true;
 }
